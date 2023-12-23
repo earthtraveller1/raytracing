d8ae14745087e7ef36b6af1ad2dca92fdefcfa85
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-12-23 16:32:00 -0500

    Created a function to convert function from our type to Raylib's type.

---
+++ src/color.zig
@@ -0,0 +1,12 @@
+const Vec3 = @import("vector.zig").Vec3;
+const raylib = @cImport(@cInclude("raylib.h"));
+const Color = Vec3;
+
+pub fn to_raylib_color(color: *const Color) raylib.Color {
+    return .{
+        .r = @intFromFloat(color.x * 255.99),
+        .g = @intFromFloat(color.y * 255.99),
+        .b = @intFromFloat(color.z * 255.99),
+        .a = 255,
+    };
+}
